a58b94dd62a9c529696a2efd92f414cd4e7c668f
alanams97 <alan_ams97@gmail.com>
2020-04-09 19:42:57 -0300

    Instalado nodemon -D

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const server = express()
 
 server.get("/", function(req, res) {
-    return res.send("Hi!")
+    return res.send("Hi, how's going?")
 })
 
 server.listen(5000, function() {
