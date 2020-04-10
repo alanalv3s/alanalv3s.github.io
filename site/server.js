7a45061a014856e30754a4d5232c390f1bc529ed
alanams97 <alan_ams97@gmail.com>
2020-04-10 19:34:23 -0300

    Migrado página Sobre para o Nunjucks

--- site/server.js
+++ site/server.js
@@ -9,11 +9,26 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/56656415?s=460&u=fdaf70b72efe06a503eaa41592f6f6e5a2344c93&v=4",
+        name: "Alan Alves",
+        role: 'Analista de Suporte e Monitoramento NOC - <a href="https://www.estabil.is/" target="_blank">Estabilis</a>',
+        description: "Estudante de programação web, focado em se tornar um programador full-stack",
+        links: [
+            { name: "GitHub", url: "https://github.com/alanalv3s"},
+            { name: "Twitter", url: "https://twitter.com/AlanAlv3s"},
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/alan-alves-silva"},
+            { name: "Email", url: "mailto: alanams97@gmail.com"},
+            { name: "Telefone", url: "tel:+5511975036861"},
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res) {
